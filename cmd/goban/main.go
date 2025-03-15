3f84aaf9e0a61733086bfacbf263c386ea6e51f7
zod-Exarion <exalted.rajubiri@gmail.com>
2025-03-15 11:51:49 +0530

    Remove useless files

--- cmd/goban/main.go
+++ cmd/goban/main.go
@@ -2,19 +2,31 @@ package goban
 
 import (
 	"goban/internal/database"
-	"goban/test"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func RunGoban() {
-	db, err := database.InitDB("database.db")
+	executablePath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	// Resolve the executable path to its absolute location
+	realpath, err := filepath.EvalSymlinks(executablePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the directory of the realpath
+	dir := filepath.Dir(realpath)
+
+	db, err := database.InitDB(filepath.Join(dir, "database.db"))
 	if err != nil {
 		log.Fatalf("Unable to initialize the database: %v", err)
 	}
 	defer db.Close()
-
-	test.RunTests(db)
 
 	if len(os.Args) > 1 {
 		gobancli := NewApp(db)
